9fecdfd5f82e33a87d0147ea19c1ddcc2b8b477c
herekim <khere220@gmail.com>
2022-10-23 21:33:08 +0900

    feat: Modify svg component

--- src/components/icon/icon.js
+++ src/components/icon/icon.js
@@ -1,16 +1,19 @@
 import Core from 'core/core';
 import { getResetStyle, getHoverIcon } from 'utils/style';
+import createTemplate, { getIcon } from 'utils/template';
 
 export default class Icon extends Core {
   constructor() {
     super();
+    this.attachShadow({ mode: 'open' });
     this.w = 10;
     this.h = 10;
     this.isHover = false;
+    this.render();
   }
 
   static get observedAttributes() {
-    return ['w', 'h', 'hover'];
+    return ['w', 'h', 'hover', 'icon'];
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -24,6 +27,9 @@ export default class Icon extends Core {
       if (newValue !== 'true' && newValue !== 'false') return;
       this.isHover = Boolean(newValue);
     }
+    if (name === 'icon') {
+      this.icon = newValue;
+    }
   }
 
   getTemplate() {
@@ -31,8 +37,23 @@ export default class Icon extends Core {
       ${getResetStyle()}
       ${this.isHover ? getHoverIcon() : ''}
       <button>
-        ${this.getIcon()}
+        ${getIcon(this.icon, this.w, this.h)}
       </button>
     `;
   }
+
+  update() {
+    this.$('svg').remove();
+    this.$('button').innerHTML = getIcon(this.icon, this.w, this.h);
+  }
+
+  connectedCallback() {
+    this.update();
+  }
+
+  render() {
+    createTemplate(this);
+  }
 }
+
+customElements.define('svg-icon', Icon);
